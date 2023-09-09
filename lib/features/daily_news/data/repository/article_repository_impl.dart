0c24861b301f55f1441c3ce19f07953b8820fc42
Novpic <juclondonome@gmail.com>
2023-09-09 09:34:34 -0500

    add gitnore to constants

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,14 +1,40 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:news_for_day/core/resources/data_state.dart';
+import 'package:news_for_day/features/daily_news/data/data_source/remote/news_api_service.dart';
 import 'package:news_for_day/features/daily_news/data/models/article.dart';
 
-import 'package:news_for_day/features/daily_news/domain/entities/article.dart';
-
+import '../../../../core/constants/constants.dart';
 import '../../domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  ArticleRepositoryImpl({required this.newsApiService});
+  final NewsApiService newsApiService;
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataError(
+          DioException(
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+            error: httpResponse.response.statusCode,
+            response: httpResponse.response,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataError(e);
+    }
   }
 }
